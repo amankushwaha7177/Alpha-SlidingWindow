898c9fa8aec1114fc2b21c512dca2b362e1d04dd
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2026-09-02 23:38:12 +0530

    m

--- _2. Pattern - 2 Variable Window/_2. Length of Longest substring  without repeating characters/src/_3B_Optimal_O_n______Direct_Jump_Of_L_Using_Index_ByMap.java
+++ _2. Pattern - 2 Variable Window/_2. Length of Longest substring  without repeating characters/src/_3B_Optimal_O_n______Direct_Jump_Of_L_Using_Index_ByMap.java
@@ -1,4 +1,4 @@
-public class _3B_Optimal_O_n______Direct_Jump_Of_L_Using_Index_ByMap____OnlyIfIndexJumpPossible {
+public class _3B_Optimal_O_n______Direct_Jump_Of_L_Using_Index_ByMap {
     /*
     Interview : Given a string, find the length of the longest substring
                 without repeating characters.
