448a78679c53c43ac3b553913dc3f4ac4c86ebc6
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2026-09-08 17:36:13 +0530

    m

---
+++ _0. Basics Of 2  Pointers/_4. Sum of 3 num Pair must be K - Gove those pair of a b c/src/Main.java
@@ -0,0 +1,58 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 1, 1, 2, 2, 2, 3, 3};
+        int k = 4;
+
+        /*
+        1. Use 2 pointer l,r to move
+        2. if currentTarget < target ===> Increase | Move l++
+           if currentTarget > target ===> Decrease | Move r--
+           if currentTarget ==target ===> Move both l++ r--
+                                          Than also skip all duplicates from both sides.
+
+        3. There are 2 ways to pause the loop
+           a. when pointers crosses each other ( l>r | r<l)
+           b. or when both they meets
+           Choose according to the question*/
+
+        // 1.
+
+        Arrays.sort(arr);
+
+        for( int i = 0; i< arr.length-2 ; i++) {
+            int l =i+1;
+            int r=arr.length-1;
+
+            while (l < r) {
+                int a = arr[l];
+                int b = arr[r];
+                int sum = arr[i] + a + b;
+                if (sum == k) {
+                    System.out.println(arr[i] + " " + a + " " + b);
+                    l++;
+                    r--;
+
+                    // These 2 lines will extra added just to skip duplicate numbers.
+                    while (arr[l] == arr[l - 1]) l++;
+                    while (arr[r] == arr[r + 1]) r--;
+                } else if (sum > k) {
+                    r--;
+                } else {
+                    l++;
+                }
+            }
+        /*
+        T=  o(n/2)
+        S = o(1)
+        */
+        }
+    }
+}
+/*
+Op:
+        2 7
+        4 5
+ */
