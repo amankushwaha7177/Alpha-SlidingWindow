6c8652c91a91b64a1c768bbf75daac030ac91c3e
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2026-09-08 15:52:34 +0530

    m

---
+++ _0. Basics Of 2  Pointers/_0. Time Complexity/src/TimeComplexity.java
@@ -0,0 +1,17 @@
+public class TimeComplexity {
+    /*
+    Q. Suppose Time is given as o(n) and n =10; What does it means ?
+    A. It means o(10) => We can perform 10 operations max
+
+       So
+       o(n)    -> max 10 operations
+       o(logn) -> max 3.3 operations
+       0(n2)   -> max 100 operations
+       o(n3)   -> max 1000 operations
+
+    Q. But what does 10^8 ?
+    A.
+
+
+     */
+}
---
+++ _0. Basics Of 2  Pointers/_1. B. Sum of 2 number Pair should be K - Give that Only pair of a b Without Duplicate/src/Main.java
@@ -0,0 +1,54 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 1, 1, 2, 2, 2, 3, 3};
+        int k = 4;
+
+        /*
+        1. Use 2 pointer l,r to move
+        2. if currentTarget < target ===> Increase | Move l++
+           if currentTarget > target ===> Decrease | Move r--
+           if currentTarget ==target ===> Move both l++ r--
+
+        3. There are 2 ways to pause the loop
+           a. when pointers crosses each other ( l>r | r<l)
+           b. or when both they meets
+           Choose according to the question*/
+
+        // 1.
+        int l =0;
+        int r=arr.length-1;
+
+        Arrays.sort(arr);
+
+        while( l < r ){
+            int a = arr[l];
+            int b = arr[r];
+            int sum = a+b;
+            if(sum == k){
+                System.out.println(a + " " +b);
+                l++;
+                r--;
+
+
+            }
+            else if(sum > k){
+                r--;
+            }
+            else{
+                l++;
+            }
+        }
+        /*
+        T=  o(n/2)
+        S = o(1)
+        */
+    }
+}
+/*
+Op:
+        2 7
+        4 5
+ */
