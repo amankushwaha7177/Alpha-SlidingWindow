e91e615b142c6387eb7e457926d2af8e29062963
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2026-09-08 13:09:21 +0530

    m

---
+++ _0. Basics Of 2  Pointers/_3. Given a Sorted -ve Pos elements - Return a sorted array of square/src/Main.java
@@ -0,0 +1,36 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {-4, -3, 0, 1, 2};
+        int l= 0;
+        int r=arr.length-1;
+
+        int[] ans = new int[arr.length];
+        int k =arr.length-1;
+
+        while(l <= r){
+            int leftSqr = arr[l] * arr[l];
+            int rightSqr = arr[r] * arr[r];
+
+            if(leftSqr > rightSqr){
+                ans[k] = leftSqr;
+                k--;
+                l++;
+            } else if ( rightSqr > leftSqr) {
+                ans[k] = rightSqr;
+                k--;
+                r--;
+            }
+            else{
+                ans[k] = leftSqr;
+                k--;
+                l++;
+            }
+        }
+        System.out.println(Arrays.toString(ans));
+    }
+}
+
+// [4, 1, 0, 9, 16]
