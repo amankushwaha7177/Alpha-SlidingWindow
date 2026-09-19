b2a8d53e32d1a851913f568a1ccc43b894736870
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2026-09-20 00:51:00 +0530

    m

--- _/_12. Prefix Sum Pattern Works with Array/piviot/src/_1_BruteForce.java
+++ _/_12. Prefix Sum Pattern Works with Array/piviot/src/_1_BruteForce.java
@@ -14,6 +14,7 @@ public class _1_BruteForce {
 
         /* Store cumulative sum from index 0 up to every index. */
         prefix[0] = arr[0]; // prefix = [ 1, ...... ]
+        /* Prefix at index 1 is far easy to calculate as compare to 0. */
 
         for(int i = 1; i < n; i++) {
             prefix[i] = prefix[i - 1] + arr[i];
