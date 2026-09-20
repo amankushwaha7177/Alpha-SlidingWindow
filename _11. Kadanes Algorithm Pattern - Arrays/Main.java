bfc757ee514192d8a2ba18c8d1678cf1ed77ffa2
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2026-09-20 17:59:45 +0530

    m

---
+++ _11. Kadanes Algorithm Pattern - Arrays/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+}
+/*
+/*
+PROBLEM TYPE                    +VE ONLY       +VE + -VE
+----------------------------------------------------------------
+Longest subarray sum <= K       Sliding        Prefix/Advanced
+Count subarrays sum <= K        Sliding        Prefix + Fenwick
+Minimum subarray sum >= K       Sliding        Prefix/Advanced
+----------------------------------------------------------------
+Count subarrays sum = K         Prefix         Prefix
+Longest subarray sum = K        Prefix         Prefix
+-----------------------------------------------------------------
+Range sum L to R                Prefix         Prefix
+-----------------------------------------------------------------
+
+Maximum subarray sum             Kadane         Kadane
+Minimum subarray sum             Kadane         Kadane
+Minimum product  sum             Kadane         Kadane
+ */
+*/
