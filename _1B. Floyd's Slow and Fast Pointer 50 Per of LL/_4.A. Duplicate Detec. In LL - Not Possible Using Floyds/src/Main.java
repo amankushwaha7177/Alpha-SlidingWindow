719bee9f07f91bdd610f5e2c87571af8ecd9b6e9
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2026-09-24 23:29:44 +0530

    m

--- _1B. Floyd's Slow and Fast Pointer 50 Per of LL/_4.A. Duplicate Detec. In LL - Not Possible Using Floyds/src/Main.java
+++ _1B. Floyd's Slow and Fast Pointer 50 Per of LL/_4.A. Duplicate Detec. In LL - Not Possible Using Floyds/src/Main.java
@@ -1,5 +1,42 @@
+import java.util.HashSet;
+
 public class Main {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(2);
+
+        System.out.println("Duplicate = " + findDuplicate(head));
+    }
+
+    static int findDuplicate(Node head) {
+        HashSet<Integer> set = new HashSet<>();
+
+        while(head != null) {
+            if(set.contains(head.data)) {
+                return head.data;
+            }
+
+            set.add(head.data);
+            head = head.next;
+        }
+
+        return -1;
     }
 }
+/*
+If you mean find a duplicate node/value in a linked list,
+Floyd's algorithm is not for finding a duplicate value. It detects a cycle.
+
+If the linked list contains repeated values, use a HashSet:
+ */
