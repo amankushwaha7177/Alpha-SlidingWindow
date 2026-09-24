8dbe1428c4fc4d04e21575d851fb663efd284294
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2026-09-24 16:32:10 +0530

    m

--- _1B. Floyd's Slow and Fast Pointer 50 Per of LL/_5. Cycle Detection In Unhappy Number/src/HappyNumber.java
+++ _1B. Floyd's Slow and Fast Pointer 50 Per of LL/_5. Cycle Detection In Unhappy Number/src/HappyNumber.java
@@ -41,7 +41,18 @@ public class HappyNumber {
             int digit = n % 10;
             sum += digit * digit;
             n = n / 10;
+
+            /*
+            n =19
+            digit = 19%10 = 9
+            n =     19/10 = 1   --> not 1.9, because n is an int. So Java performs integer division and removes the decimal part.
+
+            digit = 1 %10 = 1
+            n =     1/10  = 0
+             */
         }
+
+
 
         return sum;
     }
